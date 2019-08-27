a6f35acae14bba6c84f2e63b9636836ff814bc1a
edaniels2275 <edaniels2275@gmail.com>
2019-08-26 23:21:39 -0400

    completed draft

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -13,7 +13,9 @@ router.get('/', function(req, res) {
 
 router.get('/burgers', function(req, res) {
     burger.selectAll(function(data) {
-        var hbsObject = { burgers: data };
+        var hbsObject = {
+            burgers: data
+        };
         console.log(hbsObject);
         res.render('index', hbsObject);
     });
@@ -22,7 +24,11 @@ router.get('/burgers', function(req, res) {
 
 // Add new burger to the db.
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
         // Send back the ID of the new burger
         res.json({ id: result.insertId });
     });
@@ -34,7 +40,9 @@ router.put("/api/burgers/:id", function(req, res) {
 
     console.log("condition", condition);
 
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
             return res.status(404).end();
@@ -50,7 +58,7 @@ router.delete("/api/burgers/:id", function(req, res) {
     console.log("condition", condition);
 
     burger.deleteOne(condition, function(result) {
-        if (result.changedRows === 0) {
+        if (result.affectedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
             return res.status(404).end();
         } else {
